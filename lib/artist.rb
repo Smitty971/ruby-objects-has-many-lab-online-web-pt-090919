eac1cb6c651e0d717a404af83fd5938a384cd505
Jamal Turner <jlsmitty2273@yahoo.com>
2019-10-11 15:04:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,9 @@
 class Artist
-attr_accessor :name, :song
+attr_accessor :name, 
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def songs
@@ -13,12 +14,16 @@ attr_accessor :name, :song
     song.artist = self
   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    add_song(song)
+  def self.all
+    @@all
   end
 
-  def song_count
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
+  def self.song_count
     self.all.count
   end
 
